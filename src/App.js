04900e0fb3695b8be313ee56e4bf50d84523059c
nickng852 <nickng852@gmail.com>
2021-10-03 10:05:01 +0800

    feat: add CoinMarketCap API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  return (
-    <>
-      <div className="test">test</div>
-    </>
-  );
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+      headers: {
+        "X-CMC_PRO_API_KEY": process.env.REACT_APP_COINMARKETCAP_API_KEY,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (res) {
+        console.log(res);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }, []);
+
+  return <></>;
 };
 
 export default App;
