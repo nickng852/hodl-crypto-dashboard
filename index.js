f81b839c224c92b3fa785ae6d2ffb37b408d7bc3
nickng852 <nickng852@gmail.com>
2022-05-01 14:08:58 +0800

    deploy: heroku testing

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ app.use("/getNews", newsRoute);
 
 if (process.env.NODE_ENV === "production") {
   // Serve static files from the React frontend app
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "./client/build/index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 }
 
