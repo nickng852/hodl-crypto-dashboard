ba9233df52b2cf35c65f82ba835b667db241e9bf
nickng852 <nickng852@gmail.com>
2022-05-01 12:24:04 +0800

    deploy: heroku testing

--- index.js
+++ index.js
@@ -20,13 +20,9 @@ app.use("/getCoins", coinsRoute);
 app.use("/getCoin", coinRoute);
 app.use("/getNews", newsRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build"));
-  });
-}
+app.use(express.static("public"));
 
 // Server
 app.listen(port, () => {
