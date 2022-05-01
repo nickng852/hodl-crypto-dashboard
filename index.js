bb2e2d33c247d00a6773febe5d39f3676f786c62
nickng852 <nickng852@gmail.com>
2022-05-01 12:59:42 +0800

    deploy: heroku testing

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ app.use("/getCoin", coinRoute);
 app.use("/getNews", newsRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
 }
 
 // Server
