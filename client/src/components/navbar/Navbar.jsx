40d8129797227545acf82dec0a42a5c45d37e6a0
nickng852 <nickng852@gmail.com>
2022-05-08 11:42:10 +0800

    feat: show hamburger menu in mobile device

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import SearchBar from "../searchbar/SearchBar.jsx";
 import NavControl from "../panel/NavControl.jsx";
+import Menu from "../Menu.jsx";
 
 const NavBar = () => {
   return (
     <>
       <div className="z-10">
-        <nav className="flex items-center justify-end w-full p-4 bg-white 2xl:w-auto dark:bg-secondary">
-          {/*           <Menu /> */}
+        <nav className="flex items-center justify-between w-full p-4 bg-white 2xl:w-auto dark:bg-secondary">
+          <Menu />
           <SearchBar />
           <NavControl />
         </nav>
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -3,11 +3,6 @@ import { Link, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
   const [IsActive, setIsActive] = useState("Dashboard");
-  const [isExpand, setIsExpand] = useState(false);
-
-  const handleClick = () => {
-    setIsExpand(!isExpand);
-  };
 
   const location = useLocation();
 
@@ -36,22 +31,11 @@ const Sidebar = () => {
 
   return (
     <>
-      <main className="z-10 h-screen bg-white shadow-xl landscape:hidden landscape:sm:block portrait:hidden sm:block dark:bg-secondary">
+      <main className="z-10 h-screen bg-white shadow-xl landscape:hidden landscape:sm:block portrait:absolute portrait:-left-24 sm:block dark:bg-secondary">
         <div className="flex">
-          <div className={`h-screen ${isExpand ? "w-72" : "w-24"}`}>
-            {/* Site Name */}
-            <div className="flex items-center justify-center mt-10">
-              <span
-                className={`${
-                  isExpand ? "opacity-100" : "opacity-0"
-                } text-xl font-bold text-gray-600 dark:text-gray-100`}
-              >
-                HODL
-              </span>
-            </div>
-
+          <div className="w-24 h-screen">
             {/* Sidebar List */}
-            <nav className="px-6 mt-16 space-y-8">
+            <nav className="px-6 mt-32 space-y-8">
               {/* Dashboard */}
               <Link
                 to="/dashboard"
@@ -61,8 +45,7 @@ const Sidebar = () => {
                     ? "text-gray-900 bg-gray-100 dark:text-gray-50 dark:bg-tertiary"
                     : "text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50 hover:bg-gray-100 dark:hover:bg-tertiary"
                 }
-                ${isExpand ? "justify-start" : "justify-center"}
-                flex items-center px-3 py-2 transition-colors duration-200 rounded-lg`}
+                justify-center flex items-center px-3 py-2 transition-colors duration-200 rounded-lg`}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -78,12 +61,6 @@ const Sidebar = () => {
                     d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                   />
                 </svg>
-
-                <span
-                  className={`${isExpand ? "block" : "hidden"} ml-4 text-md`}
-                >
-                  Dashboard
-                </span>
               </Link>
 
               {/* Cryptocurrency */}
@@ -95,8 +72,7 @@ const Sidebar = () => {
                     ? "text-gray-900 bg-gray-100 dark:text-gray-50 dark:bg-tertiary"
                     : "text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50 hover:bg-gray-100 dark:hover:bg-tertiary"
                 }
-                ${isExpand ? "justify-start" : "justify-center"}
-                flex items-center px-3 py-2 transition-colors duration-200 rounded-lg`}
+              justify-center flex items-center px-3 py-2 transition-colors duration-200 rounded-lg`}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -112,12 +88,6 @@ const Sidebar = () => {
                     d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                   />
                 </svg>
-
-                <span
-                  className={`${isExpand ? "block" : "hidden"} ml-4 text-md`}
-                >
-                  Cryptocurrency
-                </span>
               </Link>
 
               {/* News */}
@@ -129,8 +99,7 @@ const Sidebar = () => {
                     ? "text-gray-900 bg-gray-100 dark:text-gray-50 dark:bg-tertiary"
                     : "text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50 hover:bg-gray-100 dark:hover:bg-tertiary"
                 }
-                ${isExpand ? "justify-start" : "justify-center"}
-                flex items-center px-3 py-2 transition-colors duration-200 rounded-lg`}
+               justify-center flex items-center px-3 py-2 transition-colors duration-200 rounded-lg`}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -146,12 +115,6 @@ const Sidebar = () => {
                     d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
                   />
                 </svg>
-
-                <span
-                  className={`${isExpand ? "block" : "hidden"} ml-4 text-md`}
-                >
-                  News
-                </span>
               </Link>
 
               {/* Setting */}
@@ -163,8 +126,7 @@ const Sidebar = () => {
                     ? "text-gray-900 bg-gray-100 dark:text-gray-50 dark:bg-tertiary"
                     : "text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50 hover:bg-gray-100 dark:hover:bg-tertiary"
                 }
-                ${isExpand ? "justify-start" : "justify-center"}
-                flex items-center px-3 py-2 transition-colors duration-200 rounded-lg`}
+              justify-center flex items-center px-3 py-2 transition-colors duration-200 rounded-lg`}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -186,60 +148,7 @@ const Sidebar = () => {
                     d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                   />
                 </svg>
-
-                <span
-                  className={`${isExpand ? "block" : "hidden"} ml-4 text-md`}
-                >
-                  Setting
-                </span>
               </Link>
-
-              <div
-                className={`${
-                  isExpand ? "justify-end" : "justify-center"
-                } flex items-center mb-10`}
-              >
-                <div
-                  className="z-10 p-2 rounded-sm cursor-pointer dark:hover:bg-tertiary hover:bg-gray-200 dark:text-gray-300"
-                  onClick={handleClick}
-                >
-                  {isExpand ? (
-                    <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-6 h-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-                        />
-                      </svg>
-                    </>
-                  ) : (
-                    <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-6 h-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M13 5l7 7-7 7M5 5l7 7-7 7"
-                        />
-                      </svg>
-                    </>
-                  )}
-                </div>
-              </div>
             </nav>
           </div>
         </div>
