623046df2ba39ab47f3f810e178593ded65d9b4a
nickng852 <nickng852@gmail.com>
2022-05-08 22:17:23 +0800

    fix: webkit 100vh issue

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -6,7 +6,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 const Layout = () => {
   return (
     <>
-      <div className="flex h-screen">
+      <div className="flex h-screen h-screen-ios">
         <Sidebar />
         <main className="flex flex-col w-full h-full overflow-hidden bg-gray-50 dark:bg-primary">
           <NavBar />
