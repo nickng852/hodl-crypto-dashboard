d73acc514a616dc55ebcee83820c9ace9439dc00
nickng852 <nickng852@gmail.com>
2022-05-22 12:15:46 +0800

    feat: add back to home page button in 404 page

--- client/src/pages/notfoundpage/NotFound.jsx
+++ client/src/pages/notfoundpage/NotFound.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
@@ -7,7 +8,7 @@ const NotFound = () => {
         <div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-24 w-24 lg:h-40 lg:w-40 text-gray-800 dark:text-gray-400"
+            class="h-24 w-24 lg:h-44 lg:w-44 text-gray-800 dark:text-gray-400"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -21,14 +22,24 @@ const NotFound = () => {
           </svg>
         </div>
 
-        <div className="flex flex-col space-y-1">
-          <span className="text-xl text-center text-gray-800 lg:text-left lg:text-3xl dark:text-gray-400">
-            404 Not Found
-          </span>
+        <div className="space-y-4">
+          <div className="flex flex-col space-y-1">
+            <span className="text-xl text-center text-gray-800 lg:text-left lg:text-3xl dark:text-gray-400">
+              404 Not Found
+            </span>
 
-          <span className="text-base font-thin text-center text-gray-800 lg:text-left lg:text-xl dark:text-gray-400">
-            Oops! We can't seem to find the page you're looking for.
-          </span>
+            <span className="text-base font-thin text-center text-gray-800 lg:text-left lg:text-xl dark:text-gray-400">
+              Oops! We can't seem to find the page you're looking for.
+            </span>
+          </div>
+
+          <div className="text-center lg:text-left">
+            <Link to="/dashboard">
+              <button className="px-4 py-2 text-sm transition ease-in border-2 border-gray-800 rounded-lg hover:text-gray-200 hover:bg-gray-800 dark:border-gray-400 lg:text-base dark:hover:text-gray-800 dark:hover:bg-gray-400 dark:text-gray-400">
+                Back to home page
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </>
