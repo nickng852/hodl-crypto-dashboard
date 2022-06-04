91670ac89de9dd568731ea4a356a3cfcde96e73e
nickng852 <nickng852@gmail.com>
2022-06-04 15:32:02 +0800

    fix: mobile issue

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <>
       <div className="z-10">
-        <nav className="flex items-center justify-between w-full px-4 py-3 bg-white lg:p-4 xl:justify-end 2xl:w-auto dark:bg-secondary">
+        <nav className="fixed flex items-center justify-between w-full px-4 py-3 bg-white lg:p-4 xl:justify-end 2xl:w-auto dark:bg-secondary">
           <Menu />
           <SearchBar />
 
