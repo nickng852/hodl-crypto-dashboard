84bd1cd8caf4aeac9f8fbe4d5628b68ec83f471b
nickng852 <nickng852@gmail.com>
2022-07-02 13:35:11 +0800

    fix: update cryptoApi baseUrl

--- client/src/services/cryptoApi.js
+++ client/src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://crypto-dashboard-heroku.herokuapp.com/"
+    ? "https://hodl-crypto-dashboard-dev.herokuapp.com/"
     : "http://localhost:3001/";
 
 export const cryptoApi = createApi({
