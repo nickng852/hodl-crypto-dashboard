9e4ef0e4ba3c4d0e3ea757b077c5c6a666544a51
Nick Ng <nick.ng@idnerd.com>
2023-11-13 12:46:29 +0800

    refactor: modify navbar shadow

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,12 +1,12 @@
-import Slidebar from "../sidebar/Slidebar.jsx";
-import SearchBar from "../searchbar/SearchBar.jsx";
 import NavControl from "../panel/NavControl.jsx";
+import SearchBar from "../searchbar/SearchBar.jsx";
+import Slidebar from "../sidebar/Slidebar.jsx";
 
 const NavBar = () => {
   return (
     <>
       <main className="z-10">
-        <nav className="flex items-center justify-between w-full px-4 py-3 bg-white shadow-sm 2xl:shadow-none lg:p-4 xl:justify-end dark:bg-secondary">
+        <nav className="flex items-center justify-between w-full px-4 py-3 bg-white shadow-sm lg:p-4 xl:justify-end dark:bg-secondary">
           <Slidebar />
           <SearchBar />
           <NavControl />
